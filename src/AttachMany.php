8eab76a70f8b6d033a52a46ecdc60347f83dc680
ericdiviney <etdiviney14@yahoo.com>
2019-02-27 10:12:00 -0600

    Remove static method call for event.

--- src/AttachMany.php
+++ src/AttachMany.php
@@ -43,11 +43,9 @@ class AttachMany extends Field
 
         $this->fillUsing(function($request, $model, $attribute, $requestAttribute) use($resource) {
             if(is_subclass_of($model, 'Illuminate\Database\Eloquent\Model')) {
-                $model::saved(function($model) use($attribute, $request) {
-                    $model->$attribute()->sync(
-                        json_decode($request->$attribute, true)
-                    );
-                });
+                $model->$attribute()->sync(
+                    json_decode($request->$attribute, true)
+                );
 
                 unset($request->$attribute);
             }
